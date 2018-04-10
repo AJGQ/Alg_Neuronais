f7d7bc84db06feeeb8e90fcbb33301947c829871
Artur Jorge <artur11jorge@gmail.com>
2018-04-10 22:53:20 +0100

    organizei o algoritmo de calcular fun_u numa funcao

--- code/euler_maruyama.py
+++ code/euler_maruyama.py
@@ -2,7 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button, RadioButtons
 
-#discretizar espaço e tempo
+lim = 100
+
+#variaveis
+h = 2.8997
+A = 2
+b = 0.08
+alfa = np.pi/10
+
+#discretizar espaco e tempo
 dx = 0.5
 lim = 100
 X = np.arange(-lim/2,lim/2,dx)
@@ -11,14 +19,6 @@ numNeu = len(X)
 T = 4
 numSteps = 100
 dt = T/numSteps
-
-#variáveis
-h = 2.8997
-A = 2
-b = 0.08
-alfa = np.pi/10
-
-fun_u = np.zeros((numSteps,numNeu))
 
 def fun_f(x):
     if(x < 0):
@@ -29,18 +29,24 @@ def fun_f(x):
 def fun_w(x):
     return A*np.exp(-b*np.abs(x))*(b*np.sin(np.abs(alfa*x)) + np.cos(alfa*x))
 
-def Integral(x,t):
-    return sum([dx*(fun_w(x-X[y])*fun_f(fun_u[t,y]-h)) for y in range(numNeu)])
+def calculate_U():
 
-#fun_u[0]=fun_S
-fun_u[0] = -0.5 + 8 * np.exp(-(X**2)/18)
+    #inicializar fun_u
+    fun_u = np.zeros((numSteps,numNeu))
 
-for i in range(1,numSteps):
-    fun_u[i] = (1-dt)*fun_u[i-1] + dt*(Integral(X,i-1) + fun_u[0])
+    fun_S = -0.5 + 20 * np.exp(-(X**2)/18)
+    fun_u[0] = fun_S
 
+    def Integral(x,t):
+        return sum([dx*(fun_w(x-X[y])*fun_f(fun_u[t,y]-h)) for y in range(numNeu)])
 
+    for i in range(1,numSteps):
+        du = dt*(-fun_u[i-1] + Integral(X,i-1))
+        fun_u[i] = du + fun_u[i-1]
 
+    return fun_u
 
+fun_u = calculate_U()
 #Plot
 fig, _ = plt.subplots()
 plt.subplots_adjust(left=0.25, bottom=0.25)
